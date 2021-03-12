b878151d8ae60bfac29c358aadc4c57d9b3fc9f4
Den <denis.batia004@gmail.com>
2021-03-13 01:43:17 +0300

    1.19

--- api/webdriver/webdriver.py
+++ api/webdriver/webdriver.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 class Ocra:
 
     def get_browser(self):
-        browser = webdriver.Remote('http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
+        browser = webdriver.Remote('http://selenium-hub:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
         return browser
 
 
